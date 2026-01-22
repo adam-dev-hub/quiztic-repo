213c924198a45c7185231fe760f3a972809dd9f2
Adam Farjeoui <adam.farjeoui2801@istic.ucar.tn>
2026-01-22 14:36:57 +0100

    Modify metro.config.js to handle .svg files
    Updated metro.config.js to ensure .svg files are loaded as raw text.

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,11 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
-/**
- * Metro configuration for Expo projects
- * @type {import('expo/metro-config').MetroConfig}
- */
 const config = getDefaultConfig(__dirname);
 
-// ✅ extend assetExts so .svg files are loaded as raw text
+// extend assetExts so .svg files are loaded as raw text
 config.resolver.assetExts.push('svg');
 
-// ✅ keep your custom minifier settings
 config.transformer.minifierConfig = {
   keep_classnames: true,
   keep_fnames: true,
